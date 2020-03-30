ca17407e45a135b8554a31cb03f680bcf9c13c40
rmkeezer <rmkeezer@yahoo.com>
2020-03-30 04:45:36 -0500

    Fix lint error

--- ibm_cloud_sdk_core/api_exception.py
+++ ibm_cloud_sdk_core/api_exception.py
@@ -14,8 +14,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 from typing import Optional
-from requests import Response
 from http import HTTPStatus
+from requests import Response
 
 
 class ApiException(Exception):
