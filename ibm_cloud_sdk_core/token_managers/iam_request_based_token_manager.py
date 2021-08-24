4c596894f64154047dae95a2d45172c3c7ee5e86
Norbert Biczo <pyrooka@users.noreply.github.com>
2021-08-24 16:43:09 +0200

    fix: make `request_payload` instance variable

--- ibm_cloud_sdk_core/token_managers/iam_request_based_token_manager.py
+++ ibm_cloud_sdk_core/token_managers/iam_request_based_token_manager.py
@@ -62,8 +62,6 @@ class IAMRequestBasedTokenManager(JWTTokenManager):
     DEFAULT_IAM_URL = 'https://iam.cloud.ibm.com'
     OPERATION_PATH = "/identity/token"
 
-    request_payload = {}
-
     def __init__(self,
                  url: Optional[str] = None,
                  client_id: Optional[str] = None,
@@ -83,6 +81,7 @@ class IAMRequestBasedTokenManager(JWTTokenManager):
         self.refresh_token = None
         self.proxies = proxies
         self.scope = scope
+        self.request_payload = {}
         super().__init__(
             self.url, disable_ssl_verification=disable_ssl_verification, token_name='access_token')
 
