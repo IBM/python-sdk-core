e76a6ffdaa7a6c64863bffc9889e8c63dda4ff8e
Norbert Biczo <pyrooka@users.noreply.github.com>
2024-01-24 12:41:55 +0100

    chore: add negative test case and other small improvements
    Signed-off-by: Norbert Biczo <pyrooka@users.noreply.github.com>

--- test_integration/test_no_ssl_verification.py
+++ test_integration/test_no_ssl_verification.py
@@ -4,11 +4,14 @@ import threading
 from http.server import HTTPServer, SimpleHTTPRequestHandler
 from ssl import PROTOCOL_TLS_SERVER, SSLContext
 
+import pytest
+from requests.exceptions import SSLError
+
 from test.test_base_service import AnyServiceV1
 from ibm_cloud_sdk_core.authenticators import NoAuthAuthenticator
 
 
-def test_no_ssl_verification():
+def test_ssl_verification():
     # Load the certificate and the key files.
     cert = os.path.join(os.path.dirname(__file__), '../resources/test_ssl.cert')
     key = os.path.join(os.path.dirname(__file__), '../resources/test_ssl.key')
@@ -17,20 +20,28 @@ def test_no_ssl_verification():
     ssl_context = SSLContext(PROTOCOL_TLS_SERVER)
     ssl_context.load_cert_chain(certfile=cert, keyfile=key)
 
-    # Create and start the server.
-    server = HTTPServer(('localhost', 3333), SimpleHTTPRequestHandler)
+    # Create and start the server on a separate thread.
+    server = HTTPServer(('127.0.0.1', 3333), SimpleHTTPRequestHandler)
     server.socket = ssl_context.wrap_socket(server.socket, server_side=True)
     t = threading.Thread(target=server.serve_forever)
     t.start()
 
-    # Now create the service and call our server via HTTPS but without SSL verification.
-    service = AnyServiceV1('2024-01-23', authenticator=NoAuthAuthenticator())
-    service.set_service_url('https://127.0.0.1:3333')
-    service.set_disable_ssl_verification(True)
-    prepped = service.prepare_request('GET', url='')
-
-    # Make the reqests, check the result and shutdown the server.
+    # We run everything in a big try-except-finally block to make sure we always
+    # shutdown the HTTP server gracefully .
     try:
+        service = AnyServiceV1('2024-01-23', service_url='https://127.0.0.1:3333', authenticator=NoAuthAuthenticator())
+        #
+        # First call the server with the default configuration.
+        # It should fail due to the invalid SSL cert.
+        assert service.disable_ssl_verification is False
+        prepped = service.prepare_request('GET', url='/')
+        with pytest.raises(SSLError):
+            res = service.send(prepped)
+
+        # Now disable the SSL verification. The request shouldn't raise any issue.
+        service.set_disable_ssl_verification(True)
+        assert service.disable_ssl_verification is True
+        prepped = service.prepare_request('GET', url='/')
         res = service.send(prepped)
         assert res is not None
     except Exception:  # pylint: disable=try-except-raise
