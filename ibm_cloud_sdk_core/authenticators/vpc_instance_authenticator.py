50f974a2ef74ba1ca60163e7f3ef7c8f5115d907
Lídia Tarcza <100163235+diatrcz@users.noreply.github.com>
2026-07-09 11:47:30 +0200

    chore: modify lint issues
    Signed-off-by: Lídia Tarcza <100163235+diatrcz@users.noreply.github.com>

--- ibm_cloud_sdk_core/authenticators/vpc_instance_authenticator.py
+++ ibm_cloud_sdk_core/authenticators/vpc_instance_authenticator.py
@@ -105,7 +105,8 @@ class VPCInstanceAuthenticator(Authenticator):
 
         if counter > 1:
             raise ValueError(
-                'At most one of "iam_profile_id", "iam_profile_crn" or "iam_profile_name" may be specified.')
+                'At most one of "iam_profile_id", "iam_profile_crn" or "iam_profile_name" may be specified.'
+            )
 
         if self.token_manager.service_version not in self.VPC_AUTH_METADATA_SERVICE_SUPPORTED_VERSIONS:
             raise ValueError(
